0fd13cdc709b97683d0587d0f5d92da01678190a
shellyln <shellyln@users.noreply.github.com>
2020-01-26 14:40:42 +0900

    Add spec codes: exported types

--- src/_spec/compiler-4.spec.ts
+++ src/_spec/compiler-4.spec.ts
@@ -8,11 +8,120 @@ import { compile }           from '../compiler';
 
 
 describe("compiler-4", function() {
-    it("compiler-", function() {
-        expect(0).toEqual(0);
+    it("compiler-exported-types-1", function() {
+        const schema = compile(`
+            export type Foo = string | number;
+            type Bar = string | number;
+        `);
+
+        {
+            expect(Array.from(schema.keys())).toEqual([
+                'Foo', 'Bar',
+            ]);
+        }
+        {
+            const rhs: TypeAssertion = {
+                name: 'Foo',
+                typeName: 'Foo',
+                kind: 'one-of',
+                oneOf: [{
+                    kind: 'primitive',
+                    primitiveName: 'string',
+                }, {
+                    kind: 'primitive',
+                    primitiveName: 'number',
+                }],
+            };
+            const ty = getType(schema, 'Foo');
+            expect(ty).toEqual(rhs);
+            expect(schema.get('Foo')?.exported).toEqual(true);
+        }
+        {
+            const rhs: TypeAssertion = {
+                name: 'Bar',
+                typeName: 'Bar',
+                kind: 'one-of',
+                oneOf: [{
+                    kind: 'primitive',
+                    primitiveName: 'string',
+                }, {
+                    kind: 'primitive',
+                    primitiveName: 'number',
+                }],
+            };
+            const ty = getType(schema, 'Bar');
+            expect(ty).toEqual(rhs);
+            expect(schema.get('Bar')?.exported).toEqual(false);
+        }
     });
-    // TODO: exported types
-    // TODO: import statement
+    it("compiler-exported-types-2", function() {
+        const schema = compile(`
+            export interface Foo {}
+            interface Bar {}
+        `);
+
+        {
+            expect(Array.from(schema.keys())).toEqual([
+                'Foo', 'Bar',
+            ]);
+        }
+        {
+            const rhs: TypeAssertion = {
+                name: 'Foo',
+                typeName: 'Foo',
+                kind: 'object',
+                members: [],
+            };
+            const ty = getType(schema, 'Foo');
+            expect(ty).toEqual(rhs);
+            expect(schema.get('Foo')?.exported).toEqual(true);
+        }
+        {
+            const rhs: TypeAssertion = {
+                name: 'Bar',
+                typeName: 'Bar',
+                kind: 'object',
+                members: [],
+            };
+            const ty = getType(schema, 'Bar');
+            expect(ty).toEqual(rhs);
+            expect(schema.get('Bar')?.exported).toEqual(false);
+        }
+    });
+    it("compiler-exported-types-3", function() {
+        const schema = compile(`
+            export enum Foo {}
+            enum Bar {}
+        `);
+
+        {
+            expect(Array.from(schema.keys())).toEqual([
+                'Foo', 'Bar',
+            ]);
+        }
+        {
+            const rhs: TypeAssertion = {
+                name: 'Foo',
+                typeName: 'Foo',
+                kind: 'enum',
+                values: [],
+            };
+            const ty = getType(schema, 'Foo');
+            expect(ty).toEqual(rhs);
+            expect(schema.get('Foo')?.exported).toEqual(true);
+        }
+        {
+            const rhs: TypeAssertion = {
+                name: 'Bar',
+                typeName: 'Bar',
+                kind: 'enum',
+                values: [],
+            };
+            const ty = getType(schema, 'Bar');
+            expect(ty).toEqual(rhs);
+            expect(schema.get('Bar')?.exported).toEqual(false);
+        }
+    });
     // TODO: doc comments (+ extends)
     // TODO: array length
     // TODO: spread and '?' length
@@ -20,4 +129,5 @@ describe("compiler-4", function() {
     // TODO: deep cherrypick and patch
     // TODO: error reporting
     // TODO: directives
+    // TODO: import statement
 });
