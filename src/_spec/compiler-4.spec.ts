ea9c6c8cad501ecf0126c7410d55b666135b9683
shellyln <shellyln@users.noreply.github.com>
2020-01-26 16:43:24 +0900

    Add spec codes: doc comment

--- src/_spec/compiler-4.spec.ts
+++ src/_spec/compiler-4.spec.ts
@@ -122,7 +122,220 @@ describe("compiler-4", function() {
             expect(schema.get('Bar')?.exported).toEqual(false);
         }
     });
-    // TODO: doc comments (+ extends)
+    it("compiler-doc-comments-1", function() {
+        const schema = compile(`
+            /* comment1 */
+            export type Foo = string | number;
+            /*
+             * comment2
+             */
+            type Bar = string | number;
+        `);
+
+        {
+            expect(Array.from(schema.keys())).toEqual([
+                'Foo', 'Bar',
+            ]);
+        }
+        {
+            const rhs: TypeAssertion = {
+                name: 'Foo',
+                typeName: 'Foo',
+                kind: 'one-of',
+                oneOf: [{
+                    kind: 'primitive',
+                    primitiveName: 'string',
+                }, {
+                    kind: 'primitive',
+                    primitiveName: 'number',
+                }],
+            };
+            const ty = getType(schema, 'Foo');
+            expect(ty).toEqual(rhs);
+            expect(schema.get('Foo')?.exported).toEqual(true);
+        }
+        {
+            const rhs: TypeAssertion = {
+                name: 'Bar',
+                typeName: 'Bar',
+                kind: 'one-of',
+                oneOf: [{
+                    kind: 'primitive',
+                    primitiveName: 'string',
+                }, {
+                    kind: 'primitive',
+                    primitiveName: 'number',
+                }],
+            };
+            const ty = getType(schema, 'Bar');
+            expect(ty).toEqual(rhs);
+            expect(schema.get('Bar')?.exported).toEqual(false);
+        }
+    });
+    it("compiler-doc-comments-2", function() {
+        const schema = compile(`
+            /** comment1 */
+            export type Foo = string | number;
+            /**
+             * comment2
+             */
+            type Bar = string | number;
+        `);
+
+        {
+            expect(Array.from(schema.keys())).toEqual([
+                'Foo', 'Bar',
+            ]);
+        }
+        {
+            const rhs: TypeAssertion = {
+                name: 'Foo',
+                typeName: 'Foo',
+                kind: 'one-of',
+                oneOf: [{
+                    kind: 'primitive',
+                    primitiveName: 'string',
+                }, {
+                    kind: 'primitive',
+                    primitiveName: 'number',
+                }],
+                docComment: 'comment1',
+            };
+            const ty = getType(schema, 'Foo');
+            expect(ty).toEqual(rhs);
+            expect(schema.get('Foo')?.exported).toEqual(true);
+        }
+        {
+            const rhs: TypeAssertion = {
+                name: 'Bar',
+                typeName: 'Bar',
+                kind: 'one-of',
+                oneOf: [{
+                    kind: 'primitive',
+                    primitiveName: 'string',
+                }, {
+                    kind: 'primitive',
+                    primitiveName: 'number',
+                }],
+                docComment: '* comment2',
+            };
+            const ty = getType(schema, 'Bar');
+            expect(ty).toEqual(rhs);
+            expect(schema.get('Bar')?.exported).toEqual(false);
+        }
+    });
+    it("compiler-doc-comments-3", function() {
+        const schema = compile(`
+            /** comment X */
+            type X = string;
+            /** comment Foo */
+            export interface Foo {
+                /** comment Foo.a */
+                a: number;
+                /** comment Foo.b */
+                b: X;
+                c: X;
+                /** comment Foo.d */
+                d: string;
+                /** comment Foo.propNames */
+                [propNames: string]: string;
+            }
+            /** comment Bar */
+            interface Bar extends Foo {
+                /** comment Bar.e */
+                e: number;
+                /** comment Bar.propNames */
+                [propNames: number]: number;
+            }
+        `);
+
+        {
+            expect(Array.from(schema.keys())).toEqual([
+                'X', 'Foo', 'Bar',
+            ]);
+        }
+        {
+            const rhs: TypeAssertion = {
+                name: 'X',
+                typeName: 'X',
+                kind: 'primitive',
+                primitiveName: 'string',
+                docComment: 'comment X',
+            };
+            const ty = getType(schema, 'X');
+            expect(ty).toEqual(rhs);
+            expect(schema.get('X')?.exported).toEqual(false);
+        }
+        {
+            const rhs: TypeAssertion = {
+                name: 'Foo',
+                typeName: 'Foo',
+                kind: 'object',
+                members: [
+                    ['a',
+                        {name: 'a', kind: 'primitive', primitiveName: 'number'},
+                        false, 'comment Foo.a',
+                    ],
+                    ['b',
+                        {name: 'b', typeName: 'X', kind: 'primitive', primitiveName: 'string', docComment: 'comment X'},
+                        false, 'comment Foo.b',
+                    ],
+                    ['c',
+                        {name: 'c', typeName: 'X', kind: 'primitive', primitiveName: 'string', docComment: 'comment X'},
+                    ],
+                    ['d',
+                        {name: 'd', kind: 'primitive', primitiveName: 'string'},
+                        false, 'comment Foo.d',
+                    ],
+                ],
+                additionalProps: [
+                    [['string'], {kind: 'primitive', primitiveName: 'string'}, false, 'comment Foo.propNames'],
+                ],
+                docComment: 'comment Foo',
+            };
+            const ty = getType(schema, 'Foo');
+            expect(ty).toEqual(rhs);
+            expect(schema.get('Foo')?.exported).toEqual(true);
+        }
+        {
+            const rhs: TypeAssertion = {
+                name: 'Bar',
+                typeName: 'Bar',
+                kind: 'object',
+                members: [
+                    ['e',
+                        {name: 'e', kind: 'primitive', primitiveName: 'number'},
+                        false, 'comment Bar.e',
+                    ],
+                    ['a',
+                        {name: 'a', kind: 'primitive', primitiveName: 'number'},
+                        true, 'comment Foo.a',
+                    ],
+                    ['b',
+                        {name: 'b', typeName: 'X', kind: 'primitive', primitiveName: 'string', docComment: 'comment X'},
+                        true, 'comment Foo.b',
+                    ],
+                    ['c',
+                        {name: 'c', typeName: 'X', kind: 'primitive', primitiveName: 'string', docComment: 'comment X'},
+                        true,
+                    ],
+                    ['d',
+                        {name: 'd', kind: 'primitive', primitiveName: 'string'},
+                        true, 'comment Foo.d',
+                    ],
+                ],
+                additionalProps: [
+                    [['string'], {kind: 'primitive', primitiveName: 'string'}, true, 'comment Foo.propNames'],
+                    [['number'], {kind: 'primitive', primitiveName: 'number'}, false, 'comment Bar.propNames'],
+                ],
+                baseTypes: [getType(schema, 'Foo') as any],
+                docComment: 'comment Bar',
+            };
+            const ty = getType(schema, 'Bar');
+            expect(ty).toEqual(rhs);
+            expect(schema.get('Bar')?.exported).toEqual(false);
+        }
+    });
     // TODO: array length
     // TODO: spread and '?' length
     // TODO: decorators
