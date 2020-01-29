db99017278717296180a6ad16124fa6c2e228939
shellyln <shellyln@users.noreply.github.com>
2020-01-30 00:17:35 +0900

    Add spec codes: error message decorators (1)

--- src/_spec/compiler-6.spec.ts
+++ src/_spec/compiler-6.spec.ts
@@ -1,5 +1,7 @@
 
 import { TypeAssertion,
+         ObjectAssertion,
+         AdditionalPropsMember,
          ValidationContext } from '../types';
 import { validate,
          getType }           from '../validator';
@@ -45,11 +47,156 @@ describe("compiler-6", function() {
             }
         `);
 
-        // TODO: type, enum, export
-
-        const ty = getType(schema, 'C');
-
-        expect(0).toEqual(0);
+        {
+            const ty = getType(schema, 'A');
+            const rhs: TypeAssertion = {
+                name: 'A',
+                typeName: 'A',
+                kind: 'object',
+                members: [
+                    ['a1', {
+                        name: 'a1',
+                        kind: 'primitive',
+                        primitiveName: 'string',
+                        messageId: 'MSG_A.a1',
+                    }],
+                    ['a2', {
+                        name: 'a2',
+                        kind: 'optional',
+                        optional: {
+                            kind: 'primitive',
+                            primitiveName: 'number',
+                        },
+                        messageId: 'MSG_A.a2',
+                    }],
+                    ['a3', {
+                        name: 'a3',
+                        kind: 'repeated',
+                        min: null,
+                        max: null,
+                        repeated: {
+                            kind: 'primitive',
+                            primitiveName: 'string',
+                        },
+                        messageId: 'MSG_A.a3',
+                    }],
+                ],
+                additionalProps: [
+                    [['string'], {
+                        'kind': 'any',
+                        'messageId': 'MSG_A.a4',
+                    }],
+                ],
+                messageId: 'MSG_A',
+            };
+            expect(ty).toEqual(rhs);
+        }
+        {
+            const ty = getType(schema, 'B');
+            const rhs: TypeAssertion = {
+                name: 'B',
+                typeName: 'B',
+                kind: 'object',
+                members: [
+                    ['b1', {
+                        name: 'b1',
+                        kind: 'primitive',
+                        primitiveName: 'boolean',
+                        messageId: 'MSG_B.b1',
+                    }],
+                    ['b2', {
+                        name: 'b2',
+                        typeName: 'A',
+                        kind: 'object',
+                        members: [...(getType(schema, 'A') as ObjectAssertion).members],
+                        additionalProps: [...((getType(schema, 'A') as ObjectAssertion).additionalProps as AdditionalPropsMember[])],
+                        messageId: 'MSG_B.b2',
+                    }],
+                ],
+                messageId: 'MSG_B',
+            };
+            expect(ty).toEqual(rhs);
+        }
+        {
+            const ty = getType(schema, 'C');
+            const rhs: TypeAssertion = {
+                name: 'C',
+                typeName: 'C',
+                kind: 'object',
+                members: [
+                    ['c1', {
+                        name: 'c1',
+                        kind: 'primitive',
+                        primitiveName: 'string',
+                        messageId: 'MSG_C.c1',
+                    }],
+                    ['a1', {
+                        name: 'a1',
+                        kind: 'primitive',
+                        primitiveName: 'string',
+                        messageId: 'MSG_A.a1',
+                    }, true],
+                    ['a2', {
+                        name: 'a2',
+                        kind: 'optional',
+                        optional: {
+                            kind: 'primitive',
+                            primitiveName: 'number',
+                        },
+                        messageId: 'MSG_A.a2',
+                    }, true],
+                    ['a3', {
+                        name: 'a3',
+                        kind: 'repeated',
+                        min: null,
+                        max: null,
+                        repeated: {
+                            kind: 'primitive',
+                            primitiveName: 'string',
+                        },
+                        messageId: 'MSG_A.a3',
+                    }, true],
+                ],
+                baseTypes: [
+                    getType(schema, 'A') as any,
+                ],
+                additionalProps: [
+                    [['string'], {
+                        kind: 'any',
+                        messageId: 'MSG_A.a4',
+                    }, true]
+                ],
+                messageId: 'MSG_C',
+            };
+            expect(ty).toEqual(rhs);
+        }
+        {
+            const ty = getType(schema, 'D');
+            const rhs: TypeAssertion = {
+                name: 'D',
+                typeName: 'D',
+                kind: 'primitive',
+                primitiveName: 'string',
+                messageId: 'MSG_D',
+            };
+            expect(ty).toEqual(rhs);
+        }
+        {
+            const ty = getType(schema, 'E');
+            const rhs: TypeAssertion = {
+                name: 'E',
+                typeName: 'E',
+                kind: 'enum',
+                values: [
+                    ['P', 0],
+                    ['Q', 1],
+                    ['R', 2],
+                ],
+                messageId: 'MSG_E',
+            };
+            expect(ty).toEqual(rhs);
+        }
+        // TODO: export
     });
     // TODO: error message decorators + error reporting
 });
