c3685e0b9afaf1134ae1fc485ef7ea2dc3d34edd
shellyln <shellyln@users.noreply.github.com>
2020-01-30 06:17:52 +0900

    Add spec codes: error message decorators (2)

--- src/_spec/compiler-6.spec.ts
+++ src/_spec/compiler-6.spec.ts
@@ -20,15 +20,19 @@ describe("compiler-6", function() {
                 a1: string;
                 @msgId('MSG_A.a2')
                 a2?: number;
+                /** Comment A.a3 */
                 @msgId('MSG_A.a3')
                 a3: string[];
+                /** Comment A.a4 */
                 @msgId('MSG_A.a4')
                 [propNames: string]: any;
             }
+            /** Comment B */
             @msgId('MSG_B')
             interface B {
                 @msgId('MSG_B.b1')
                 b1: boolean;
+                /** Comment B.b2 */
                 @msgId('MSG_B.b2')
                 b2: A;
             }
@@ -37,8 +41,10 @@ describe("compiler-6", function() {
                 @msgId('MSG_C.c1')
                 c1: string;
             }
+            /** Comment D */
             @msgId('MSG_D')
             type D = string;
+            /** Comment E */
             @msgId('MSG_E')
             enum E {
                 P,
@@ -48,7 +54,13 @@ describe("compiler-6", function() {
         `);
 
         {
+            expect(Array.from(schema.keys())).toEqual([
+                'A', 'B', 'C', 'D', 'E',
+            ]);
+        }
+        {
             const ty = getType(schema, 'A');
+            expect(false).toEqual(schema.get('A')?.exported as any);
             const rhs: TypeAssertion = {
                 name: 'A',
                 typeName: 'A',
@@ -79,13 +91,13 @@ describe("compiler-6", function() {
                             primitiveName: 'string',
                         },
                         messageId: 'MSG_A.a3',
-                    }],
+                    }, false, 'Comment A.a3'],
                 ],
                 additionalProps: [
                     [['string'], {
                         'kind': 'any',
                         'messageId': 'MSG_A.a4',
-                    }],
+                    }, false, 'Comment A.a4'],
                 ],
                 messageId: 'MSG_A',
             };
@@ -93,6 +105,7 @@ describe("compiler-6", function() {
         }
         {
             const ty = getType(schema, 'B');
+            expect(false).toEqual(schema.get('B')?.exported as any);
             const rhs: TypeAssertion = {
                 name: 'B',
                 typeName: 'B',
@@ -111,14 +124,16 @@ describe("compiler-6", function() {
                         members: [...(getType(schema, 'A') as ObjectAssertion).members],
                         additionalProps: [...((getType(schema, 'A') as ObjectAssertion).additionalProps as AdditionalPropsMember[])],
                         messageId: 'MSG_B.b2',
-                    }],
+                    }, false, 'Comment B.b2'],
                 ],
                 messageId: 'MSG_B',
+                docComment: 'Comment B',
             };
             expect(ty).toEqual(rhs);
         }
         {
             const ty = getType(schema, 'C');
+            expect(false).toEqual(schema.get('C')?.exported as any);
             const rhs: TypeAssertion = {
                 name: 'C',
                 typeName: 'C',
@@ -155,7 +170,7 @@ describe("compiler-6", function() {
                             primitiveName: 'string',
                         },
                         messageId: 'MSG_A.a3',
-                    }, true],
+                    }, true, 'Comment A.a3'],
                 ],
                 baseTypes: [
                     getType(schema, 'A') as any,
@@ -164,7 +179,7 @@ describe("compiler-6", function() {
                     [['string'], {
                         kind: 'any',
                         messageId: 'MSG_A.a4',
-                    }, true]
+                    }, true, 'Comment A.a4']
                 ],
                 messageId: 'MSG_C',
             };
@@ -172,17 +187,20 @@ describe("compiler-6", function() {
         }
         {
             const ty = getType(schema, 'D');
+            expect(false).toEqual(schema.get('D')?.exported as any);
             const rhs: TypeAssertion = {
                 name: 'D',
                 typeName: 'D',
                 kind: 'primitive',
                 primitiveName: 'string',
                 messageId: 'MSG_D',
+                docComment: 'Comment D',
             };
             expect(ty).toEqual(rhs);
         }
         {
             const ty = getType(schema, 'E');
+            expect(false).toEqual(schema.get('E')?.exported as any);
             const rhs: TypeAssertion = {
                 name: 'E',
                 typeName: 'E',
@@ -193,10 +211,214 @@ describe("compiler-6", function() {
                     ['R', 2],
                 ],
                 messageId: 'MSG_E',
+                docComment: 'Comment E',
             };
             expect(ty).toEqual(rhs);
         }
-        // TODO: export
+    });
+    it("compiler-error-reporting-1", function() {
+        const schema = compile(`
+            @msgId('MSG_A')
+            export interface A {
+                @msgId('MSG_A.a1')
+                a1: string;
+                @msgId('MSG_A.a2')
+                a2?: number;
+                /** Comment A.a3 */
+                @msgId('MSG_A.a3')
+                a3: string[];
+                /** Comment A.a4 */
+                @msgId('MSG_A.a4')
+                [propNames: string]: any;
+            }
+            /** Comment B */
+            @msgId('MSG_B')
+            export interface B {
+                @msgId('MSG_B.b1')
+                b1: boolean;
+                /** Comment B.b2 */
+                @msgId('MSG_B.b2')
+                b2: A;
+            }
+            @msgId('MSG_C')
+            export interface C extends A {
+                @msgId('MSG_C.c1')
+                c1: string;
+            }
+            /** Comment D */
+            @msgId('MSG_D')
+            export type D = string;
+            /** Comment E */
+            @msgId('MSG_E')
+            export enum E {
+                P,
+                Q,
+                R,
+            }
+        `);
+
+        {
+            expect(Array.from(schema.keys())).toEqual([
+                'A', 'B', 'C', 'D', 'E',
+            ]);
+        }
+        {
+            const ty = getType(schema, 'A');
+            expect(true).toEqual(schema.get('A')?.exported as any);
+            const rhs: TypeAssertion = {
+                name: 'A',
+                typeName: 'A',
+                kind: 'object',
+                members: [
+                    ['a1', {
+                        name: 'a1',
+                        kind: 'primitive',
+                        primitiveName: 'string',
+                        messageId: 'MSG_A.a1',
+                    }],
+                    ['a2', {
+                        name: 'a2',
+                        kind: 'optional',
+                        optional: {
+                            kind: 'primitive',
+                            primitiveName: 'number',
+                        },
+                        messageId: 'MSG_A.a2',
+                    }],
+                    ['a3', {
+                        name: 'a3',
+                        kind: 'repeated',
+                        min: null,
+                        max: null,
+                        repeated: {
+                            kind: 'primitive',
+                            primitiveName: 'string',
+                        },
+                        messageId: 'MSG_A.a3',
+                    }, false, 'Comment A.a3'],
+                ],
+                additionalProps: [
+                    [['string'], {
+                        'kind': 'any',
+                        'messageId': 'MSG_A.a4',
+                    }, false, 'Comment A.a4'],
+                ],
+                messageId: 'MSG_A',
+            };
+            expect(ty).toEqual(rhs);
+        }
+        {
+            const ty = getType(schema, 'B');
+            expect(true).toEqual(schema.get('B')?.exported as any);
+            const rhs: TypeAssertion = {
+                name: 'B',
+                typeName: 'B',
+                kind: 'object',
+                members: [
+                    ['b1', {
+                        name: 'b1',
+                        kind: 'primitive',
+                        primitiveName: 'boolean',
+                        messageId: 'MSG_B.b1',
+                    }],
+                    ['b2', {
+                        name: 'b2',
+                        typeName: 'A',
+                        kind: 'object',
+                        members: [...(getType(schema, 'A') as ObjectAssertion).members],
+                        additionalProps: [...((getType(schema, 'A') as ObjectAssertion).additionalProps as AdditionalPropsMember[])],
+                        messageId: 'MSG_B.b2',
+                    }, false, 'Comment B.b2'],
+                ],
+                messageId: 'MSG_B',
+                docComment: 'Comment B',
+            };
+            expect(ty).toEqual(rhs);
+        }
+        {
+            const ty = getType(schema, 'C');
+            expect(true).toEqual(schema.get('C')?.exported as any);
+            const rhs: TypeAssertion = {
+                name: 'C',
+                typeName: 'C',
+                kind: 'object',
+                members: [
+                    ['c1', {
+                        name: 'c1',
+                        kind: 'primitive',
+                        primitiveName: 'string',
+                        messageId: 'MSG_C.c1',
+                    }],
+                    ['a1', {
+                        name: 'a1',
+                        kind: 'primitive',
+                        primitiveName: 'string',
+                        messageId: 'MSG_A.a1',
+                    }, true],
+                    ['a2', {
+                        name: 'a2',
+                        kind: 'optional',
+                        optional: {
+                            kind: 'primitive',
+                            primitiveName: 'number',
+                        },
+                        messageId: 'MSG_A.a2',
+                    }, true],
+                    ['a3', {
+                        name: 'a3',
+                        kind: 'repeated',
+                        min: null,
+                        max: null,
+                        repeated: {
+                            kind: 'primitive',
+                            primitiveName: 'string',
+                        },
+                        messageId: 'MSG_A.a3',
+                    }, true, 'Comment A.a3'],
+                ],
+                baseTypes: [
+                    getType(schema, 'A') as any,
+                ],
+                additionalProps: [
+                    [['string'], {
+                        kind: 'any',
+                        messageId: 'MSG_A.a4',
+                    }, true, 'Comment A.a4']
+                ],
+                messageId: 'MSG_C',
+            };
+            expect(ty).toEqual(rhs);
+        }
+        {
+            const ty = getType(schema, 'D');
+            expect(true).toEqual(schema.get('D')?.exported as any);
+            const rhs: TypeAssertion = {
+                name: 'D',
+                typeName: 'D',
+                kind: 'primitive',
+                primitiveName: 'string',
+                messageId: 'MSG_D',
+                docComment: 'Comment D',
+            };
+            expect(ty).toEqual(rhs);
+        }
+        {
+            const ty = getType(schema, 'E');
+            expect(true).toEqual(schema.get('E')?.exported as any);
+            const rhs: TypeAssertion = {
+                name: 'E',
+                typeName: 'E',
+                kind: 'enum',
+                values: [
+                    ['P', 0],
+                    ['Q', 1],
+                    ['R', 2],
+                ],
+                messageId: 'MSG_E',
+                docComment: 'Comment E',
+            };
+            expect(ty).toEqual(rhs);
+        }
     });
     // TODO: error message decorators + error reporting
 });
