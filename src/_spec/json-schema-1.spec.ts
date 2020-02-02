37a36c8cd0e1fa36a161a1f46bc5743b3c48dfac
shellyln <shellyln@users.noreply.github.com>
2020-02-02 18:18:00 +0900

    Add spec codes: JSON Schema

--- src/_spec/json-schema-1.spec.ts
+++ src/_spec/json-schema-1.spec.ts
@@ -134,4 +134,253 @@ describe("json-schema-1", function() {
             message: 'should be string',
         }]);
     });
+    it("json-schema-1-2", function() {
+        const z = compile(`
+            type Foo = string;
+            interface A {
+                a: Foo;
+                @range(3, 5)
+                b: number;
+                @maxLength(3)
+                c: Foo;
+                [propNames: /^[e-f]$/]: number;
+                i?: string;
+            }
+            interface B {
+                x: A;
+            }
+        `);
+        const schema = generateJsonSchemaObject(z);
+
+        const ajv = new Ajv({allErrors: true});
+        // tslint:disable-next-line: no-implicit-dependencies
+        ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'));
+        const ajvValidate = ajv.addSchema(schema).getSchema('#/definitions/B');
+
+        expect(ajvValidate({x: {a: 'z', b: 5, c: '123'}})).toEqual(true);
+
+        expect(ajvValidate({x: {b: 5, c: '123'}})).toEqual(false);
+        expect(ajvValidate.errors).toEqual([{
+            keyword: 'required',
+            dataPath: '.x',
+            schemaPath: '#/required',
+            params: { missingProperty: 'a' },
+            message: 'should have required property \'a\'',
+        }]);
+
+        expect(ajvValidate({x: {a: 1, b: 5, c: '123'}})).toEqual(false);
+        expect(ajvValidate.errors).toEqual([{
+            keyword: 'type',
+            dataPath: '.x.a',
+            schemaPath: '#/definitions/Foo/type',
+            params: { type: 'string' },
+            message: 'should be string',
+        }]);
+    });
+    it("json-schema-1-3", function() {
+        const z = compile(`
+            type Foo = string;
+            type A = {
+                a: Foo,
+                @range(3, 5)
+                b: number,
+                @maxLength(3)
+                c: Foo,
+                [propNames: /^[e-f]$/]: number,
+                i?: string,
+            };
+            type B = {
+                x: A,
+            };
+        `);
+        const schema = generateJsonSchemaObject(z);
+
+        const ajv = new Ajv({allErrors: true});
+        // tslint:disable-next-line: no-implicit-dependencies
+        ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'));
+        const ajvValidate = ajv.addSchema(schema).getSchema('#/definitions/B');
+
+        expect(ajvValidate({x: {a: 'z', b: 5, c: '123'}})).toEqual(true);
+
+        expect(ajvValidate({x: {b: 5, c: '123'}})).toEqual(false);
+        expect(ajvValidate.errors).toEqual([{
+            keyword: 'required',
+            dataPath: '.x',
+            schemaPath: '#/required',
+            params: { missingProperty: 'a' },
+            message: 'should have required property \'a\'',
+        }]);
+
+        expect(ajvValidate({x: {a: 1, b: 5, c: '123'}})).toEqual(false);
+        expect(ajvValidate.errors).toEqual([{
+            keyword: 'type',
+            dataPath: '.x.a',
+            schemaPath: '#/definitions/Foo/type',
+            params: { type: 'string' },
+            message: 'should be string',
+        }]);
+    });
+    it("json-schema-1-4", function() {
+        const z = compile(`
+            type Foo = string|number;
+        `);
+        const schema = generateJsonSchemaObject(z);
+
+        const ajv = new Ajv({allErrors: true});
+        // tslint:disable-next-line: no-implicit-dependencies
+        ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'));
+        const ajvValidate = ajv.addSchema(schema).getSchema('#/definitions/Foo');
+
+        expect(ajvValidate(11)).toEqual(true);
+        expect(ajvValidate('11')).toEqual(true);
+
+        expect(ajvValidate(true)).toEqual(false);
+        expect(ajvValidate.errors).toEqual([{
+            keyword: 'type',
+            dataPath: '',
+            schemaPath: '#/anyOf/0/type',
+            params: { type: 'string' },
+            message: 'should be string',
+        }, {
+            keyword: 'type',
+            dataPath: '',
+            schemaPath: '#/anyOf/1/type',
+            params: { type: 'number' },
+            message: 'should be number',
+        }, {
+            keyword: 'anyOf',
+            dataPath: '',
+            schemaPath: '#/anyOf',
+            params: {},
+            message: 'should match some schema in anyOf',
+        }]);
+    });
+    it("json-schema-1-5", function() {
+        const z = compile(`
+            type Foo = number[];
+        `);
+        const schema = generateJsonSchemaObject(z);
+
+        const ajv = new Ajv({allErrors: true});
+        // tslint:disable-next-line: no-implicit-dependencies
+        ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'));
+        const ajvValidate = ajv.addSchema(schema).getSchema('#/definitions/Foo');
+
+        expect(ajvValidate([11, 13])).toEqual(true);
+
+        expect(ajvValidate([11, 13, '17'])).toEqual(false);
+        expect(ajvValidate.errors).toEqual([{
+            keyword: 'type',
+            dataPath: '[2]',
+            schemaPath: '#/items/type',
+            params: { type: 'number' },
+            message: 'should be number',
+        }]);
+    });
+    it("json-schema-1-6", function() {
+        const z = compile(`
+            type Foo = (number|boolean)[];
+        `);
+        const schema = generateJsonSchemaObject(z);
+
+        const ajv = new Ajv({allErrors: true});
+        // tslint:disable-next-line: no-implicit-dependencies
+        ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'));
+        const ajvValidate = ajv.addSchema(schema).getSchema('#/definitions/Foo');
+
+        expect(ajvValidate([11, 13, false])).toEqual(true);
+
+        expect(ajvValidate([11, 13, '17'])).toEqual(false);
+        expect(ajvValidate.errors).toEqual([{
+            keyword: 'type',
+            dataPath: '[2]',
+            schemaPath: '#/items/anyOf/0/type',
+            params: { type: 'number' },
+            message: 'should be number',
+        }, {
+            keyword: 'type',
+            dataPath: '[2]',
+            schemaPath: '#/items/anyOf/1/type',
+            params: { type: 'boolean' },
+            message: 'should be boolean',
+        }, {
+            keyword: 'anyOf',
+            dataPath: '[2]',
+            schemaPath: '#/items/anyOf',
+            params: {},
+            message: 'should match some schema in anyOf',
+        }]);
+    });
+    it("json-schema-1-6", function() {
+        const z = compile(`
+            type Foo = [number, boolean];
+        `);
+        const schema = generateJsonSchemaObject(z);
+
+        const ajv = new Ajv({allErrors: true});
+        // tslint:disable-next-line: no-implicit-dependencies
+        ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'));
+        const ajvValidate = ajv.addSchema(schema).getSchema('#/definitions/Foo');
+
+        expect(ajvValidate([11, false])).toEqual(true);     // NOTE: In JSON Schema, compile to `(number | boolean)[]`
+        expect(ajvValidate([false, 11, 13])).toEqual(true);
+
+        expect(ajvValidate([11, 13, '17'])).toEqual(false);
+        expect(ajvValidate.errors).toEqual([{
+            keyword: 'type',
+            dataPath: '[2]',
+            schemaPath: '#/items/anyOf/0/type',
+            params: { type: 'number' },
+            message: 'should be number',
+        }, {
+            keyword: 'type',
+            dataPath: '[2]',
+            schemaPath: '#/items/anyOf/1/type',
+            params: { type: 'boolean' },
+            message: 'should be boolean',
+        }, {
+            keyword: 'anyOf',
+            dataPath: '[2]',
+            schemaPath: '#/items/anyOf',
+            params: {},
+            message: 'should match some schema in anyOf',
+        }]);
+    });
+    it("json-schema-1-7", function() {
+        const z = compile(`
+            enum Foo {
+                A = 'a',
+                B = 10,
+                C = 'b',
+            }
+        `);
+        const schema = generateJsonSchemaObject(z);
+
+        const ajv = new Ajv({allErrors: true});
+        // tslint:disable-next-line: no-implicit-dependencies
+        ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'));
+        const ajvValidate = ajv.addSchema(schema).getSchema('#/definitions/Foo');
+
+        expect(ajvValidate('a')).toEqual(true);
+        expect(ajvValidate(10)).toEqual(true);
+        expect(ajvValidate('b')).toEqual(true);
+
+        expect(ajvValidate(11)).toEqual(false);
+        expect(ajvValidate.errors).toEqual([{
+            keyword: 'enum',
+            dataPath: '',
+            schemaPath: '#/enum',
+            params: { allowedValues: ['a', 10, 'b'] },
+            message: 'should be equal to one of the allowed values',
+        }]);
+
+        expect(ajvValidate('c')).toEqual(false);
+        expect(ajvValidate.errors).toEqual([{
+            keyword: 'enum',
+            dataPath: '',
+            schemaPath: '#/enum',
+            params: { allowedValues: ['a', 10, 'b'] },
+            message: 'should be equal to one of the allowed values',
+        }]);
+    });
 });
